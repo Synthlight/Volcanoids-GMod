f99d02853ff7bc2d44514c9407090cb3f8c967d2
Gregory Brutsche <gregory@mail.brutsches.local>
2020-01-07 01:48:39 -0500

    Show all deposit markers on the map.

---
+++ Patches/TrainUpgradesPatch.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace GMod.Patches {
+    [HarmonyPatch]
+    public class TrainUpgradesPatch {
+        [HarmonyTargetMethod]
+        public static MethodBase TargetMethod() {
+            return typeof(TrainUpgrades).GetProperty(nameof(TrainUpgrades.Radar), BindingFlags.Public | BindingFlags.Instance)?.GetGetMethod();
+        }
+
+        [HarmonyPrefix]
+        public static bool Prefix(ref float __result) {
+            __result = 5000f;
+            return false;
+        }
+    }
+}

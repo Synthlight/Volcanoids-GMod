93d203ff625807f7d7b0217f6c01dac938a99232
Gregory Brutsche <gregory@mail.brutsches.local>
2020-01-07 01:48:23 -0500

    Reveal whole map.

---
+++ Patches/MapTravelUiPatch.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GMod.Patches {
+    [HarmonyPatch]
+    public class MapTravelUiPatch {
+        [HarmonyTargetMethod]
+        public static MethodBase TargetMethod() {
+            return typeof(MapTravelUi).GetMethod("Awake", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix() {
+            Player.Local.TryGetComponentSafe<MapMemory>()?.RevealAll();
+        }
+    }
+}

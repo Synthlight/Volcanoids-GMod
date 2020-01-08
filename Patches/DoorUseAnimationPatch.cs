c2f03e18cc9756151e14efca396308defc4f798b
Gregory Brutsche <gregory@mail.brutsches.local>
2020-01-07 21:38:42 -0500

    Auto-close doors players open.

---
+++ Patches/DoorUseAnimationPatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GMod.Patches {
+    [HarmonyPatch]
+    public class DoorUseAnimationPatch {
+        [HarmonyTargetMethod]
+        public static MethodBase TargetMethod() {
+            return typeof(DoorUseAnimation).GetMethod("Use", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix(ref DoorUseAnimation __instance, ref GameObject user) {
+            if (!__instance.IsOpen) return;
+            var instance = __instance;
+            var o = user;
+            new Routine(instance, delegate {
+                if (!instance.IsOpen) return;
+                TargetMethod().Invoke(instance, new object[] {o});
+            }, new WaitForSeconds(2f)).Restart();
+            __instance.CloseSoon();
+        }
+    }
+}

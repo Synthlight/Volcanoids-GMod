b060b0e844e30a28424e71889dad97013c3d881a
Gregory Brutsche <gregory@mail.brutsches.local>
2020-01-07 21:38:11 -0500

    Double power plant output and prevent sleeping at energy threshold.

---
+++ Patches/PowerPlantPatch.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace GMod.Patches {
+    [HarmonyPatch]
+    public class PowerPlantPatch {
+        [HarmonyTargetMethod]
+        public static MethodBase TargetMethod() {
+            return typeof(PowerPlant).GetMethod("Awake", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        [HarmonyPrefix]
+        public static void Prefix(ref PowerPlant __instance) {
+            __instance.EnergyPerSecond *= 2f;
+            __instance.StopThreshold = 2f;
+        }
+    }
+}
